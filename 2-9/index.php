874d11d10d1c0a4ea5cd90eeb1f87ff518d20613
itokaori <acero510701@gmail.com>
2022-05-10 13:44:59 +0900

    PHP2-9課題提出
    PHP2-9の課題を提出いたします。

---
+++ 2-9/index.php
@@ -0,0 +1,18 @@
+<?php
+  $num =0;
+  for($i=0; $i<101; $i++) {
+      if($i % 3 == 0 && $i % 5 == 0 ) {
+          echo "FizzBuzz!";
+          echo '<br>';
+      }elseif ($i % 3 == 0) {
+          echo "Fizz!";
+          echo '<br>';
+      }elseif ($i % 5 == 0) {
+          echo "Buzz!";
+          echo '<br>';
+      }else{
+          echo $i;
+          echo '<br>';
+      }
+  }
+?>
