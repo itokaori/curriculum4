36e7726d27191c8e813310bf740a1e87e110ce03
itokaori <acero510701@gmail.com>
2022-05-14 23:26:49 +0900

    PHP2-12課題提出
    2-12の課題を提出します

---
+++ 2-12/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$prize = $_POST['prize'];
+$quantity = $_POST['quantity'];
+?>
+<p>お名前：<?php echo $my_name; ?></p>
+<p>ご希望景品：<?php echo $prize; ?></p>
+<p>個数：<?php echo $quantity; ?></p>
